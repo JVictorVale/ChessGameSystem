f195bd1c77c0a43f93beadd61a0d9fbfd6a222cf
Victor Vale <joaovictorvale06@gmail.com>
2023-05-12 11:22:29 -0300

    Criando a Classe Posicao

---
+++ Chess/Tabuleiro/Posicao.cs
@@ -0,0 +1,17 @@
+namespace Tabuleiro
+{
+
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        
+        
+    }
+}
