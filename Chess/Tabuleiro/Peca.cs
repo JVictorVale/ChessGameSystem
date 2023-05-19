7043d589de6b42b49e018ff30fdb9b382b068acf
Victor Vale <joaovictorvale06@gmail.com>
2023-05-19 13:59:09 -0300

    Testes iniciais com movimentos possiveis das peças

--- Chess/Tabuleiro/Peca.cs
+++ Chess/Tabuleiro/Peca.cs
@@ -1,7 +1,7 @@
 ﻿namespace Tabuleiro
 {
 
-    public class Peca
+    public abstract class Peca
     {
         public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
@@ -20,5 +20,7 @@
         {
             QteMovimentos++;
         }
+
+        public abstract bool[,] MovimentosPossiveis();
     }
 }
