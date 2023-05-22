5490020ec52324c374d5781b2b910d108c555e17
Victor Vale <joaovictorvale06@gmail.com>
2023-05-22 10:01:12 -0300

    Controlando as peças capturadas

--- Chess/Tela.cs
+++ Chess/Tela.cs
@@ -1,12 +1,47 @@
 ﻿using Chess.Xadrez;
 using Tabuleiro;
-using static Tabuleiro.Tabuleiro;
+using System.Collections.Generic;
+
 
 namespace Chess
 {
 
     public class Tela
     {
+        public static void ImprimirPartida(PartidaDeXadrez partida)
+        {
+            ImprimirTabuleiro(partida.Tab);
+            Console.WriteLine();
+            ImprimirPecasCapturadas(partida);
+            Console.WriteLine();
+            Console.WriteLine("Turno: " + partida.Turno);
+            Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+        }
+
+        public static void ImprimirPecasCapturadas(PartidaDeXadrez partida)
+        {
+            Console.WriteLine("Peças capturadas: ");
+            Console.Write("Brancas: ");
+            ImprimirConjunto(partida.PecasCapturadas(Cor.Branca));
+            Console.WriteLine();
+            Console.Write("Pretas: ");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            ImprimirConjunto(partida.PecasCapturadas(Cor.Preta));
+            Console.ForegroundColor = aux;
+            Console.WriteLine();
+        }
+
+        public static void ImprimirConjunto(HashSet<Peca> conjunto)
+        {
+            Console.Write("[");
+            foreach (Peca x in conjunto)
+            {
+                Console.Write(x + " ");
+            }
+            Console.Write("]");
+        }
+        
         public static void ImprimirTabuleiro(Tabuleiro.Tabuleiro tab)
         {
             for (int i = 0; i < tab.Linhas; i++)
